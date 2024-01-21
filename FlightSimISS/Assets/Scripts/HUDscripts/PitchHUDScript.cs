8e0160b62b13a5af663e4a93d7d4fe37cef1fcef
mihaelLeskovic <127503981+mihaelLeskovic@users.noreply.github.com>
2024-01-21 13:31:39 +0100

    b

--- FlightSimISS/Assets/Scripts/HUDscripts/PitchHUDScript.cs
+++ FlightSimISS/Assets/Scripts/HUDscripts/PitchHUDScript.cs
@@ -1,21 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PitchUI : MonoBehaviour {
     [SerializeField] private GameObject horizonIndicatorPrefab;
+    [SerializeField] private GameObject regularBarPrefab;
     [SerializeField] private Transform planeTransform;
     [SerializeField] private Camera camera;
+    [SerializeField] private int barDistance;
+    [SerializeField] private int max;
+    [SerializeField] private int maxBarHeight;
 
     private GameObject horizonGO;
     private RectTransform horizonTransform;
 
-    private float maxY = 128;
+    private Dictionary<int, GameObject> barGOs;
+    private Dictionary<int, RectTransform> barTransforms;
     
     // Start is called before the first frame update
     void Start() {
-        horizonGO = Instantiate(horizonIndicatorPrefab, transform);
-        horizonTransform = horizonGO.GetComponent<RectTransform>();
+
+        barGOs = new Dictionary<int, GameObject>();
+        barTransforms = new Dictionary<int, RectTransform>();
+        
+        for (int i = -max; i <= max; i += barDistance) {
+            if (i == 0) {
+                CreateBar(i, horizonIndicatorPrefab);
+            }else CreateBar(i, regularBarPrefab);
+        }
     }
 
     // Update is called once per frame
@@ -24,20 +39,24 @@ public class PitchUI : MonoBehaviour {
         float roll = planeTransform.eulerAngles.z;
 
         transform.localEulerAngles = new Vector3(0, 0, -roll);
+        
 
-        float angle = Mathf.DeltaAngle(pitch, 0);
-        float position = GetPosition(ConvertAngle(angle));
-        
-        if (Mathf.Abs(angle) < 90f &&  Mathf.Abs(position) < maxY) {
-            //if bar position is within bounds
-            Vector3 pos = horizonTransform.localPosition;
-            horizonTransform.localPosition = new Vector3(pos.x, position, pos.z);
-            horizonGO.SetActive(true);
-        
-            // if (bar.bar != null) bar.bar.UpdateRoll(roll);
-        } else {
-            horizonGO.SetActive(false);
+        for (int barAngle = -max; barAngle <= max; barAngle += barDistance) {
+            // if(barAngle==0) continue;
+
+            float angle = Mathf.DeltaAngle(pitch, barAngle);
+            float position = GetPosition(ConvertAngle(angle));
+
+            if (Mathf.Abs(angle) < 90f && Mathf.Abs(position) < maxBarHeight) {
+                Vector3 pos = barTransforms[barAngle].localPosition;
+                barTransforms[barAngle].localPosition = new Vector3(pos.x, position, pos.z);
+                barGOs[barAngle].SetActive(true);
+            }
+            else {
+                barGOs[barAngle].SetActive(false);
+            }
         }
+        
     }
 
     float GetPosition(float angle) {
@@ -56,5 +75,22 @@ public class PitchUI : MonoBehaviour {
         }
 
         return angle;
+    }
+
+    void CreateBar(int angle, GameObject prefab)
+    {
+        GameObject barGO = Instantiate(prefab, transform);
+        RectTransform barTransform = barGO.GetComponent<RectTransform>();
+
+        if(angle!=0) SetNumber(barGO, angle);
+        barGOs.Add(angle, barGO);
+        barTransforms.Add(angle, barTransform);
+    }
+
+    void SetNumber(GameObject barGO, int angle) {
+        Text[] texts = barGO.GetComponentsInChildren<Text>();
+        foreach(Text text in texts) {
+            text.text = angle.ToString();
+        }
     }
 }
